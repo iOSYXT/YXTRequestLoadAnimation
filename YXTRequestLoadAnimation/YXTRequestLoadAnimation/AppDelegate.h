f2f6b79ee71eefcf0cd21ddcc5974c27cd226709
XT Y <yangxiaotong@yangxiaotongdemacbook-pro.local>
2016-10-14 19:38:37 +0800

    初始化项目

---
+++ YXTRequestLoadAnimation/YXTRequestLoadAnimation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YXTRequestLoadAnimation
+//
+//  Created by 杨小童 on 16/10/14.
+//  Copyright © 2016年 YXT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YXTRequestLoadAnimation/YXTRequestLoadAnimation/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  YXTRequestLoadAnimation
+//
+//  Created by 杨小童 on 16/10/14.
+//  Copyright © 2016年 YXT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
